a880ee38a4f9b06678a11fdd9b8f1eb737ffedb6
iitKD <valar5736@gmail.com>
2023-12-22 11:31:08 +0530

    disabling filesystem module from loading

--- new.py
+++ new.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
 import subprocess
-def disable_filesystem_loading(l_mname):
+
+def configure_kernel_module(l_mname):
+
     # Setting Module to be Not Loadable
     install_line = f"install {l_mname} /bin/false\n"
     modprobe_conf_path = f"/etc/modprobe.d/{l_mname}.conf"
@@ -18,22 +20,24 @@ def disable_filesystem_loading(l_mname):
             f.write(install_line)
 
     # Unloading the Module
-    if subprocess.Popen(["lsmod", "|", "grep", l_mname], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True).returncode == 0:
+    lsmod_output = subprocess.run(["lsmod"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    if l_mname in lsmod_output.stdout:
         print(f" - unloading module \"{l_mname}\"")
         subprocess.run(["modprobe", "-r", l_mname])
 
     # Deny Listing the Module
-    blacklist_line = f"blacklist-{l_mname}\n"
+    blacklist_line = f"blacklist {l_mname}\n"
+    modprobe_blacklist_path = "/etc/modprobe.d/blacklist.conf"
 
     try:
-        with open("/etc/modprobe.d/*", 'r') as f:
+        with open(modprobe_blacklist_path, 'r') as f:
             if not any(blacklist_line in line for line in f):
-                with open(modprobe_conf_path, 'a') as f:
+                with open(modprobe_blacklist_path, 'a') as f:
                     print(f" - deny listing \"{l_mname}\"")
                     f.write(blacklist_line)
     except FileNotFoundError:
-        with open(modprobe_conf_path, 'w') as f:
+        with open(modprobe_blacklist_path, 'w') as f:
             print(f" - deny listing \"{l_mname}\"")
             f.write(blacklist_line)
 
-# the above function can be called for "cramfs", "squashfs" and "udf" for Ubuntu.
+
