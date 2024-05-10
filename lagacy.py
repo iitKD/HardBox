909560638a67647df2b851f089d226a6e979218d
Kundan <142815479+iitKD@users.noreply.github.com>
2024-05-10 14:39:49 +0530

    Update lagacy.py

--- lagacy.py
+++ lagacy.py
@@ -1,6 +1,5 @@
 import subprocess
-import shutil
-
+import re
 def edit_fstab_add_nosuid():
     fstab_path = 'new.txt'
     flag=0
@@ -24,10 +23,33 @@ def edit_fstab_add_nosuid():
             f.writelines(fstab_content)
 
 def remount_tmp():
-    # Run the mount command to remount /tmp
     subprocess.run(['mount', '-o', 'remount', '/tmp'])
-
 
 edit_fstab_add_nosuid()
 
+def create_partition(device, size):
+    try:
+
+        fdisk_input = f"n\np\n\n\n+{size}\n\nw\n".encode()
+        fdisk_process = subprocess.Popen(['fdisk', device], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        fdisk_process.communicate(fdisk_input)
+        fdisk_output = subprocess.check_output(['fdisk', '-l', device]).decode('utf-8')
+        partition_name = re.findall(r'^{}(\d+)'.format(device), fdisk_output, re.MULTILINE)[-1] 
+        return partition_name
+    except Exception as e:
+        print("Error:", e)
+        return None
+
+
+device = '/dev/sda'
+size  = "20G"#
+direc = "/tmp"
+partition_name = create_partition(device, size)
+if partition_name:
+    print("New partition created:", device + partition_name)
+    deviceName = device+partition_name
+    makefilesystem = subprocess.run(["mkfs.ext4",deviceName], capture_output=True)
+    makedir = subprocess.run(["mkdir", f"/{direc[-3:]}"],capture_output=True)
+    if makefilesystem.returncode ==0:
+        mountFs = subprocess.run(["mount", deviceName,f"/{direc[-3:]}"])
 
